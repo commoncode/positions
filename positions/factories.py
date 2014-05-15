7d364aff9cce21aa22bc0e54c743fb0617dacd7a
Y3K <dboy.y3k@gmail.com>
2014-05-15 00:57:47 -0500

    Factories && Commands

--- positions/factories.py
+++ positions/factories.py
@@ -13,3 +13,14 @@ class PositionFactory(factory.django.DjangoModelFactory):
     FACTORY_DJANGO_GET_OR_CREATE = ('title', )
 
     title = factory.LazyAttribute(lambda o: words(2, common=False).title())
+
+    @factory.post_generation
+    def links(self, create, extracted, **kwargs):
+        if not create:
+            # Simple build, do nothing.
+            return
+
+        if extracted:
+            # A list of groups were passed in, use them
+            for link in extracted:
+                self.links.add(link)
--- positions/management/commands/create_positions.py
+++ positions/management/commands/create_positions.py
@@ -1,6 +1,9 @@
 from random import choice
 
+from django.core.management import call_command
 from django.core.management.base import BaseCommand, CommandError
+
+from menus.models import Link
 
 from ...factories import PositionFactory
 
@@ -12,7 +15,16 @@ class Command(BaseCommand):
         print "Creating Positions"
 
         titles = ('Header', 'Aside', 'Feature', 'Footer')
+        links = Link.objects.all()
+
+        if not links.exists():
+            call_command('create_menus')
 
         for title in titles:
-            PositionFactory(title=title)
+            random_links = []
+
+            for j in range(3):
+                random_links.append(choice(links))
+
+            PositionFactory(title=title, links=random_links)
             print "Added Position: {}".format(title)
